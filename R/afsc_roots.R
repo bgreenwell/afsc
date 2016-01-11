5a250113cc3a2fdffd58d1b3c1befb9006b71163
Brandon Greenwell <greenwell.brandon@gmail.com>
2016-01-11 09:57:55 -0500

    added option allow_X to validate functions

--- R/afsc_roots.R
+++ R/afsc_roots.R
@@ -3,6 +3,7 @@
 #' Extract or strip off AFSC prefixes and suffixes (if any).
 #'
 #' @param x A character vector containing AFSCs.
+#' @param allow_X Logical indicating whether or not the "X" placeholder is allowed. Default is \code{FALSE}.
 #' @return \code{strip_roots} returns the original vector of AFSCs with all of
 #'   the prefixes and suffixes (if any) removed. The functions \code{get_prefix} and
 #'   \code{get_suffix} return, respectively, the prefix and suffix (if any) from
@@ -14,10 +15,10 @@
 #' strip_roots(afscs)
 #' get_prefix(afscs)
 #' get_suffix(afscs)
-strip_roots <- function(x) {
+strip_roots <- function(x, allow_X = allow_X) {
 
   # Logical vectors indicating which elements contain a prefix/suffix (if any)
-  invalid <- !is_valid_afsc(x)
+  invalid <- !is_valid_afsc(x, allow_X = allow_X)
   prefixes <- grepl("^[a-zA-Z]", x)
   suffixes <- grepl("[a-zA-Z]$", x)
 
@@ -41,7 +42,7 @@ strip_roots <- function(x) {
 #' @rdname afsc_roots
 #' @export
 get_prefix <- function(x) {
-  invalid <- !is_valid_afsc(x)
+  invalid <- !is_valid_afsc(x, allow_X = allow_X)
   prefixes <- grepl("^[a-zA-Z]", x)
   x[prefixes] <- substr(x[prefixes], start = 1, stop = 1)
   x[!prefixes] <- "none"
@@ -53,7 +54,7 @@ get_prefix <- function(x) {
 #' @rdname afsc_roots
 #' @export
 get_suffix <- function(x) {
-  invalid <- !is_valid_afsc(x)
+  invalid <- !is_valid_afsc(x, allow_X = allow_X)
   suffixes <- grepl("[a-zA-Z]$", x)
   x[suffixes] <- substr(x[suffixes], start = nchar(x[suffixes]),
                         stop = nchar(x[suffixes]))
